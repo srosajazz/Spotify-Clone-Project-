ccf07e9c43651514f7e8e4ac1fa05aa77fcaa34f
Sergio Rosa <scarlosjazz@gmail.com>
2019-06-05 16:49:45 -0400

    Implementing mainpage with GlobalStyle

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+// import GlobalStyle from "./styles/global";
+import GlobalStyle from "./styles/global";
+
+const App = () => (
+  <div>
+    <GlobalStyle />
+    <h1>Hello World</h1>
+  </div>
+);
+
+export default App;
